282d438da93736588f7f42b3e092eb8c95cced2c
shaibaoj <wu2yong3jinh@163.com>
2023-01-05 15:01:22 +0800

    no message

--- web/assets/common.95b51a45.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index.042a2974.js";const s={list:"/api/common/channel/items",getGoodsItems:"/api/common/goods/list",getGoodsView:"/api/common/goods/view"};function i(o){return t({url:s.list,method:"post",data:o})}export{i as g};
