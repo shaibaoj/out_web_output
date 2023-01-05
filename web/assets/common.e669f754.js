8297fae956bf99414b48e418b740390d93ad7528
shaibaoj <wu2yong3jinh@163.com>
2023-01-05 12:46:47 +0800

    no message

--- web/assets/common.e669f754.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index.004badce.js";const s={list:"/api/common/channel/items",getGoodsItems:"/api/common/goods/list",getGoodsView:"/api/common/goods/view"};function i(o){return t({url:s.list,method:"post",data:o})}export{i as g};
