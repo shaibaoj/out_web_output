21bd9c39d96c04451cb64d32ab1a0b45546788b9
shaibaoj <wu2yong3jinh@163.com>
2025-05-19 23:42:19 +0800

    no message

--- web/assets/illustration.3726e6b7.js
+++ web/assets/illustration.3726e6b7.js
@@ -1,1 +1,1 @@
-const t="https://static.youdanhui.com/web_output/web/assets/illustration.bbfd1da0.svg";export{t as _};
+const t="https://cdn.jsdelivr.net/gh/shaibaoj/out_web_output/web/assets/illustration.bbfd1da0.svg";export{t as _};
